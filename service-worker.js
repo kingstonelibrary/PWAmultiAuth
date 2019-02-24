79ec57a85552f88a6a0ea3f5017f9a4da8e64d28
ukonn_admin <UKONN@Mac-mini.local>
2019-02-24 11:06:40 +0900

    TC

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,13 @@
 // service-worker.js
-self.addEventListener('install', function(e) {
+
+var urlsToCache = [
+  '/',
+  '/p.html'
+];
+
+  self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
+    return install(e);
   });
   
   self.addEventListener('activate', function(e) {
@@ -9,3 +16,24 @@ self.addEventListener('install', function(e) {
   
   // 現状では、この処理を書かないとService Workerが有効と判定されないようです
   self.addEventListener('fetch', function(event) {});
+  
+  // HTML側からのメッセージハンドラ
+  self.addEventListener('message', function(event) {
+    return install(event);
+  });
+  
+  const install = (event) => {
+    return event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          urlsToCache.map(url => {
+            return fetch(new Request(url)).then(response => {
+              return cache.put(url, response);
+            });
+          })
+        })
+        .catch(function(err) {
+          console.log(err);
+        })
+    );
+  }
